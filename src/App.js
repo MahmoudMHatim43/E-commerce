46ce97f7d5b3df0213714ed32aa1e28d18997067
MahmoudMHatim43 <mahmoudmhatim@gmail.com>
2024-08-28 03:21:23 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -8,21 +9,82 @@ import Shop from "./pages/Shop";
 import ProductDetails from "./pages/ProductDetails";
 import Cart from "./pages/Cart";
 import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
+  const location = useLocation();
+
   return (
     <div className="grid grid-cols-12 grid-rows-[auto_1fr_auto] gap-x-4 text-light-t1 dark:text-dark-t1 bg-light-main dark:bg-dark-main">
+      <ScrollToTop />
       <header className="col-span-12">
         <Navbar />
       </header>
       <main className="col-span-12">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/products/:productId" element={<ProductDetails />} />
-          <Route path="/cart" element={<Cart />} />
-        </Routes>
+        <AnimatePresence mode="wait">
+          <Routes location={location} key={location.pathname}>
+            <Route
+              path="/"
+              element={
+                <motion.div
+                  initial={{ opacity: 0, y: -50 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: -50 }}
+                  transition={{ duration: 0.5 }}>
+                  <Home />
+                </motion.div>
+              }
+            />
+            <Route
+              path="/login"
+              element={
+                <motion.div
+                  initial={{ opacity: 0, y: 50 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: 50 }}
+                  transition={{ duration: 0.5 }}>
+                  <Login />
+                </motion.div>
+              }
+            />
+            <Route
+              path="/shop"
+              element={
+                <motion.div
+                  initial={{ opacity: 0, y: 50 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: 50 }}
+                  transition={{ duration: 0.5 }}>
+                  <Shop />
+                </motion.div>
+              }
+            />
+            <Route
+              path="/products/:productId"
+              element={
+                <motion.div
+                  initial={{ opacity: 0, y: 50 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: 50 }}
+                  transition={{ duration: 0.5 }}>
+                  <ProductDetails />
+                </motion.div>
+              }
+            />
+            <Route
+              path="/cart"
+              element={
+                <motion.div
+                  initial={{ opacity: 0, y: 50 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: 50 }}
+                  transition={{ duration: 0.5 }}>
+                  <Cart />
+                </motion.div>
+              }
+            />
+          </Routes>
+        </AnimatePresence>
       </main>
       <footer className="col-span-12 bg-light-second dark:bg-dark-second">
         <Footer />
--- src/components/PromoCard.jsx
+++ src/components/PromoCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
+import { NavLink } from "react-router-dom";
 function PromoCard({ product, discount }) {
   return (
     <>
@@ -9,20 +10,22 @@ function PromoCard({ product, discount }) {
         className="relative flex flex-col gap-4 w-[250px] p-4
          dark:bg-zinc-800 font-Nuntio border dark:border-none
          rounded-2xl shadow-[0_0_10px_0_rgba(0,0,0,0.2)] transition-shadow duration-300 cursor-pointer">
-        <img
-          src={product.images[0] || "image-not-found.png"}
-          alt="promo image"
-          className="rounded-xl shadow-xl"
-        />
-        <span className="text-lg font-bold">{product.title}</span>
-        <div className="flex flex-col gap-1">
-          <span className="text-md line-through tracking-wide">
-            ${product.price}
-          </span>
-          <span className="text-md font-bold tracking-wide">
-            ${(product.price - product.price * discount).toFixed(2)}
-          </span>
-        </div>
+        <NavLink to={`/products/${product.id}`}>
+          <img
+            src={product.images[0] || "image-not-found.png"}
+            alt="promo image"
+            className="rounded-xl shadow-xl"
+          />
+          <span className="text-lg font-bold">{product.title}</span>
+          <div className="flex flex-col gap-1">
+            <span className="text-md line-through tracking-wide">
+              ${product.price}
+            </span>
+            <span className="text-md font-bold tracking-wide">
+              ${(product.price - product.price * discount).toFixed(2)}
+            </span>
+          </div>
+        </NavLink>
         <span className="absolute top-2 left-2 p-2 bg-red-600 text-white text-sm font-bold rounded">
           %{(discount * 100).toFixed(0)} Off
         </span>
